e50df2819223c58fa1e879eb37d6e91ce4b2c8ea
ErikM <er8540ma-s@student.lu.se>
2022-09-08 18:56:09 +0200

    Känner mig klar

--- MatlabExercise1.m
+++ MatlabExercise1.m
@@ -17,9 +17,9 @@ for i = 1:49
     sig = bin2dec(bstr); %Choose signal from Apam alternatives
     plot(x, Apam(sig + 1)*grc, 'blue')
     hold on
+    sumAreaPam = abs(sumAreaPam + trapz(x, Apam(sig + 1)*grc));
     x = x + 1;
     i = i + 1;
-    sumAreaPam = sumAreaPam + trapz(x, Apam(sig + 1)*grc);
 end
 
 %% 4-PSK
@@ -29,16 +29,16 @@ sig1 = cos(8*pi*x + vl(1));
 sig2 = cos(8*pi*x + vl(2));
 sig3 = cos(8*pi*x + vl(3));
 sig4 = cos(8*pi*x + vl(4));
-signals = [sig1 sig2 sig3 sig4];
-
+signals1 = [sig1 sig2 sig3 sig4];
+sumAreaPsk = 0;
 
 for i = 1:49
     bstr = strcat(b(i), b(i+1));
-    sig = bin2dec(bstr); %Choose signal from Apam alternatives
+    sig = bin2dec(bstr); %Choose signal from alternatives
     
-    plot(x, grc * signals(sig + 1), 'blue')
+    plot(x, grc * signals1(sig + 1), 'blue')
     hold on
-    
+    sumAreaPsk = abs(sumAreaPsk + trapz(x, grc * signals1(sig + 1)));
     x = x + 1;
     i = i + 1;
 end
@@ -48,6 +48,7 @@ end
 
 Apam = [-3 -1 1 3];
 x = linspace(0,1);
+sumAreaQam = 0;
 for i = 1:47
     bstr1 = strcat(b(i), b(i+1));
     sig1 = bin2dec(bstr1); %Choose signal from Apam alternatives
@@ -55,16 +56,34 @@ for i = 1:47
     sig2 = bin2dec(bstr2);
     plot(x, Apam(sig1 + 1)*grc + Apam(sig2 + 1)*grc, 'blue')
     hold on
+    
+    sumAreaQam = abs(sumAreaQam + trapz(x, Apam(sig1 + 1)*grc + Apam(sig2 + 1)*grc));
     x = x + 1;
     i = i + 1;
 end
 
 %% 4-FSK
+x = linspace(0,1);
+sumAreaFsk = 0;
 
-
+for i = 1:49
+    bstr = strcat(b(i), b(i+1));
+    sig = bin2dec(bstr) + 1; %Choose frequency multiplier
+    plot(x, cos(2*pi*x*sig), 'blue')
+    hold on
+    
+    sumAreaFsk = abs(sumAreaFsk + trapz(x, cos(2*pi*x*sig)));
+    x = x + 1;
+    i = i + 1;
+    
+end
 
 
 %% b
 % Average symbol energy should be integral / 50
 
+EsPam = sumAreaPam * sumAreaPam / 50;
+EsPsk = sumAreaPsk * sumAreaPsk / 50;
+EsQam = sumAreaQam * sumAreaQam / 50;
+EsFsk = sumAreaFsk * sumAreaFsk / 50;
 
